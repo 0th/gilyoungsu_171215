9044557cd16c588e4c5e739985350237d42f3868
Chansik Yun <chansigi@gmail.com>
2016-11-08 05:47:06 +0900

    경로 수정

--- fc-web/routes/index.js
+++ fc-web/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var request = require('request')
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    request.get("http://fc-api-app/web", function (err, response, body) {
+    request.get("http://api.fandomcup.com/web", function (err, response, body) {
         var data = JSON.parse(body);
         for (var i = 0; i < data.data.length; i++)
             if (data.data[i]['firstUser'] == null) data.data[i]['firstUser'] = [];
