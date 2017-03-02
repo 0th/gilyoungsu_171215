2dc38e1680c4dd9d18370e833c8f8f6d679cf661
Chansik Yun <chansigi@gmail.com>
2017-03-02 13:15:28 +0900

    fixed bug

--- fc-web/routes/index.js
+++ fc-web/routes/index.js
@@ -6,7 +6,7 @@ var request = require('request')
 router.get('/', function (req, res, next) {
     request.get("http://api.fandomcup.com/web", function (err, response, body) {
         var data = JSON.parse(body);
-        for (var i = 0; i < data.data.length; i++)
+        for (var i = 0; i < data.length; i++)
             if (data.data[i]['firstUser'] == null) data.data[i]['firstUser'] = [];
 
         //console.log(data.data);
